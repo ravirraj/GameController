cd5d07c1d83c8c9f18ecb4e11c80ab17f8e2fb7d
Ravi <ravirrajpatil@gmail.com>
2023-05-04 19:58:37 +0530

    GameControllerFristCommit

---
+++ index.js
@@ -0,0 +1,17 @@
+let menulist = document.getElementById("menu-list");
+
+
+menulist.style.maxHeight = "0px";
+
+function togglemenu(){
+
+    if(menulist.style.maxHeight == "0px")
+    {
+        menulist.style.maxHeight = "130px"
+    }
+
+    else
+    {
+        menulist.style.maxHeight = "0px"
+    }
+}
